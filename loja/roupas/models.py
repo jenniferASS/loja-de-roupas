7e94c813a3b13f455caae0c4a293e63e82ea3240
jenniferASS <135033739+jenniferASS@users.noreply.github.com>
2024-03-15 17:56:24 -0300

    Update models.py

--- loja/roupas/models.py
+++ loja/roupas/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.nome
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    imagem = models.ImageField(upload_to='produtos/')
+    disponivel = models.BooleanField(default=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
+
